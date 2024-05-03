8c857fc208c8770559b76f5cbb447aaa2dd0bab9
Sai Sahith <131525241+SaiSahithPolimera@users.noreply.github.com>
2024-05-03 22:16:03 +0530

    Add path to images

--- index.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    document.querySelector('.right-container').classList.add('animate');
-    document.querySelector('.left-container').classList.add('animate');
-    document.querySelector('.about').classList.add('animate');
-}
-
---
+++ js/index.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  document.querySelector('.right-container').classList.add('animate');
+  document.querySelector('.left-container').classList.add('animate');
+  document.querySelector('.about').classList.add('animate');
+}
