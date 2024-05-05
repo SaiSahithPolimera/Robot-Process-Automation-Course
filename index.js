3c01cbea23ef51d7669454b88dbb40babbb2abfc
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2024-05-05 14:55:45 +0530

    Added resposive and necesorry changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+let exploreButton = document.getElementById("exploreXButton");
+let  explore = document.getElementById("explore");
 window.onload = function () {
     document.querySelector('.right-container').classList.add('animate');
     document.querySelector('.left-container').classList.add('animate');
     document.querySelector('.about').classList.add('animate');
-}
+    document.querySelector('.explore').classList.remove('d-none');
+};
+exploreButton.onclick = function () {
+    explore.classList.add("d-none");
+    exploreButton.classList.add("testimonial-card")
+
+};
 
