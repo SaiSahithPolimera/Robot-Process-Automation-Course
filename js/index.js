cb6412128d3bda468519064f2c625f702bdc9217
Sai Sahith <131525241+SaiSahithPolimera@users.noreply.github.com>
2024-05-08 18:56:27 +0530

    Made serveral sections responsive

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ window.onload = function () {
   document.querySelector(".left-container").classList.add("animate");
   document.querySelector(".about").classList.add("animate");
 };
+
 var swiper = new Swiper(".slide-container", {
   slidesPerView: 4,
   spaceBetween: 20,
@@ -24,12 +25,19 @@ var swiper = new Swiper(".slide-container", {
   breakpoints: {
     0: {
       slidesPerView: 1,
+      slidesPerGroup: 1
+    },
+    360: {
+      slidesPerView: 1,
+      slidesPerGroup: 1
     },
     520: {
       slidesPerView: 2,
+      slidesPerGroup: 2
     },
     768: {
-      slidesPerView: 3,
+      slidesPerView: 2,
+      slidesPerGroup: 2
     },
     1000: {
       slidesPerView: 4,
